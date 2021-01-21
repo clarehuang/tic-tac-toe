d84dc0fcbb36433845a613b831df5ed8ad956d1a
Clare Huang <clarehuang1126@gmail.com>
2021-01-21 01:17:54 -0800

    clean up

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { fireEvent, render, screen, cleanup, act, getByLabelText } from '@testing-library/react'
+import React from 'react'
+import { fireEvent, render, cleanup, act } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import App from './App'
 
